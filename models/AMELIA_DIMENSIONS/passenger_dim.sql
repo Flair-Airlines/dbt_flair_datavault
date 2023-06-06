b82f737d0c68cea51f075107dae7186bf4e24d90
prudhvinadh.koduri@flyflair.com <prudhvinadh.koduri@flyflair.com>
2023-06-06 19:22:43 +0000

    Written cte for passenger_dim

--- models/AMELIA_DIMENSIONS/passenger_dim.sql
+++ models/AMELIA_DIMENSIONS/passenger_dim.sql
@@ -5,9 +5,16 @@
         unique_key = 'PASSENGER_SURROGATE_ID_HASH'
     )
 }}
-select 
+WITH province as(
+    select PROVINCE_SURROGATE_ID_HASH,LNG_PROVINCE_ID_NMBR from {{ ref('province_dim') }}
+),
+
+country as(
+    select COUNTRY_SURROGATE_ID_HASH,LNG_COUNTRY_ID_NMBR from {{ ref('country_dim') }}
+)
+select
 {{ dbt_utils.generate_surrogate_key([
-     'DTM_DOB_DATE','STR_LAST_NAME','STR_NAME_KEY','STR_FIRST_NAME','STR_MIDDLE_NAME'
+     'PA.LNG_PAX_ID_NMBR'
 ]) }} as PASSENGER_SURROGATE_ID_HASH,
 LNG_PAX_ID_NMBR,
 STR_PRE_BOARD,
@@ -36,10 +43,16 @@ CURRENT_TIMESTAMP() AS INSERTED_TS,
 to_timestamp('1900-01-01','YYYY-MM-DD') as EFFECTIVE_DATETIME,
 NULL AS EXPIRY_DATETIME,
 {{ dbt_utils.generate_surrogate_key([
-     'DTM_DOB_DATE','STR_LAST_NAME','STR_NAME_KEY','STR_FIRST_NAME','STR_MIDDLE_NAME'
+     'PA.LNG_PAX_ID_NMBR'
 ]) }} as ORIGINAL_SURROGATE_ID_HASH,
 PD.PROVINCE_SURROGATE_ID_HASH,			
 CD.COUNTRY_SURROGATE_ID_HASH	
 from {{ source('PSS_AMELIARES_DBO', 'TBL_PAX') }} PA
-LEFT JOIN {{ ref('province_dim') }} PD ON PD.LNG_PROVINCE_ID_NMBR = PA.LNG_PROVINCE_ID_NMBR
-LEFT JOIN {{ ref('country_dim') }} CD ON CD.LNG_COUNTRY_ID_NMBR = PA.LNG_COUNTRY_ID_NMBR
+LEFT JOIN province PD ON PD.LNG_PROVINCE_ID_NMBR = PA.LNG_PROVINCE_ID_NMBR
+LEFT JOIN country CD ON CD.LNG_COUNTRY_ID_NMBR = PA.LNG_COUNTRY_ID_NMBR
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+ where INSERTED_TS > IFNULL((select max(INSERTED_TS) from {{ this }}),'1900-01-01')
+
+{% endif %}
