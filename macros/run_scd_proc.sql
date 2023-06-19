6f80f5522813dd29ee5c6e8ba45c489adbb3aacb
saravanakarthikeyan.shanmugasundaram@flyflair.com <saravanakarthikeyan.shanmugasundaram@flyflair.com>
2023-06-19 19:45:08 +0000

    Update PROC_STG_SKED_DETAIL_SCD

--- macros/run_scd_proc.sql
+++ macros/run_scd_proc.sql
@@ -2,4 +2,5 @@
   {% do run_query("call DATAVAULT_DEV.DV_AMELIA.PROC_DV_D_SKED_DETAILS();") %}
   {% do run_query("call DATAVAULT_DEV.DV_AMELIA.PROC_DV_D_RES_LEGS();") %}
   {% do run_query("call DATAVAULT_DEV.DV_AMELIA.PROC_DV_D_RES_SEGMENTS();") %}
+  {% do run_query("call STAGE_DEV.AMELIA.PROC_STG_SKED_DETAIL_SCD();") %}
 {% endmacro %}
