475e492a2dc5110619197f910b2a92f3e3381e50
saravanakarthikeyan.shanmugasundaram@flyflair.com <saravanakarthikeyan.shanmugasundaram@flyflair.com>
2023-07-25 12:34:10 +0000

    Adding PROC_ STG_PAX_ANCILLARY_CHARGES and PROC_STG_FLIGHT_SEGMENTS_TO_BAGS Procedures

--- macros/run_mis_scd_proc.sql
+++ macros/run_mis_scd_proc.sql
@@ -1,3 +1,5 @@
 {% macro run_mis_scd_proc() %}
   {% do run_query("call STAGE_DEV.MIS_DBO.PROC_STG_PAX();") %}
+  {% do run_query("call STAGE_DEV.MIS_DBO.PROC_STG_PAX_ANCILLARY_CHARGES();") %}
+  {% do run_query("call STAGE_DEV.MIS_DBO.PROC_STG_FLIGHT_SEGMENTS_TO_BAGS();") %}
 {% endmacro %}
