0ba947b665f43c9c6e3ef37689940bd45b636d93
Zoe Zhang <ruiying.zhang@flyflair.com>
2023-08-14 14:23:30 +0000

    change filter and change refund date and time to MT time for testing

--- models/IOATAWARE/VW_MONTHLY_REVENUE.sql
+++ models/IOATAWARE/VW_MONTHLY_REVENUE.sql
@@ -4,17 +4,17 @@
     )
 }}
 
-select a."Flight MST Date",a.NET_CHRG as "Daily Base Revenue",b.NET_CHRG as "Daily Ancillary Revenue",(a.NET_CHRG+b.NET_CHRG) "Total" from 
+select a."Flight Date",a.NET_CHRG as "Daily Base Revenue",b.NET_CHRG as "Daily Ancillary Revenue",(a.NET_CHRG+b.NET_CHRG) "Total" from 
 
-( select TO_VARCHAR(TO_DATE("Flight MST Date"),'MM/DD/YYYY') AS "Flight MST Date" ,"Category",sum("Net Charge") NET_CHRG
+( select TO_VARCHAR(TO_DATE("Flight Date"),'MM/DD/YYYY') AS "Flight Date" ,"Category",sum("Net Charge") NET_CHRG
 from {{ ref('VW_REVENUE_RAW_DATA') }}
 where "Category"='Base'
 group by 1,2
 ) as a join 
 
-(select TO_VARCHAR(TO_DATE("Flight MST Date"),'MM/DD/YYYY') AS "Flight MST Date" ,"Category",sum("Net Charge") NET_CHRG
+(select TO_VARCHAR(TO_DATE("Flight Date"),'MM/DD/YYYY') AS "Flight Date" ,"Category",sum("Net Charge") NET_CHRG
 from {{ ref('VW_REVENUE_RAW_DATA') }}
 where "Category"='Ancillary' 
 group by 1,2
-) as b on a."Flight MST Date"=b."Flight MST Date"
+) as b on a."Flight Date"=b."Flight Date"
 --where b."Flight Date" = '05/01/2023'
