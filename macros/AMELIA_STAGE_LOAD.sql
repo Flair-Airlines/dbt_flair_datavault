336158e616fc037c44b0eda24b891a038f0b8781
dineshkumar-sundaramoorthy-flair <141269075+dineshkumar-sundaramoorthy-flair@users.noreply.github.com>
2023-09-12 19:07:05 +0530

    Update AMELIA_STAGE_LOAD.sql
    Signed-off-by: dineshkumar-sundaramoorthy-flair <141269075+dineshkumar-sundaramoorthy-flair@users.noreply.github.com>

--- macros/AMELIA_STAGE_LOAD.sql
+++ macros/AMELIA_STAGE_LOAD.sql
@@ -52,4 +52,6 @@
   {% do run_query("call STAGE_DEV.AMELIA_DBO.PROC_STG_GL_PAYMENTS_REFUND();") %}
   {% do run_query("call STAGE_DEV.AMELIA_DBO.PROC_STG_CC_TRACK();") %}
   {% do run_query("call STAGE_DEV.AMELIA_DBO.PROC_STG_GL_CREDITCARD_PAYMENT();") %}
+  {% do run_query("call STAGE_DEV.AMELIA_DBO.PROC_STG_GL_PAYMENTS();") %}
+  {% do run_query("call STAGE_DEV.AMELIA_DBO.PROC_STG_GL_PAYMENT_CC_XREF();") %}
 {% endmacro %}
